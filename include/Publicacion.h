134121882e553ac25acc0fe30697d5cf51678b75
rouzombie <nacho3137@gmail.com>
2025-06-15 00:33:01 -0300

    Update

--- include/Publicacion.h
+++ include/Publicacion.h
@@ -23,8 +23,8 @@ public:
     void setAP(AdministraPropiedad* ap);
     void desactivar();
     void activar();
-bool cumpleFiltros(tipopublicacion TipoPublicacion ,preciominimo float, preciomaximo float)
-    bool mismotipo(tipo tipoInmueble)
+bool cumpleFiltros(tipopublicacion TipoPublicacion ,preciominimo float, preciomaximo float;
+    bool mismotipo(tipo tipoInmueble);
     DTPublicacion getDatos();
 };
 
