e10b6c25f9fb4c3511b528ba16753abcd2032137
Tobainax <tobiasmartin2@hotmail.com>
2025-06-15 18:30:49 -0300

    A

--- include/IAltaUsuario.h
+++ include/IAltaUsuario.h
@@ -14,10 +14,10 @@ class IAltaUsuario {
         virtual bool altaCliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento) = 0;
         virtual bool altaPropietario(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string cuentaBancaria, std::string telefono) = 0;
         virtual bool altaInmobiliaria(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string direccion, std::string url, std::string telefono) = 0; 
-        virtual std::set<DTUsuario::DTUsuario> listarPropietarios() = 0;
+        virtual std::set<DTUsuario*> listarPropietarios() = 0;
         virtual void representarPropietario(std::string nicknamePropietario) = 0; 
         virtual void finalizarAltaUsuario() = 0;
-        virtual std::set<DTUsuario::DTUsuario> listarInmobiliarias() = 0;
+        virtual std::set<DTUsuario*> listarInmobiliarias() = 0;
         virtual std::set<DTInmuebleAdministrado> listarInmueblesAdministrados(std::string nicknameInmobiliaria) = 0; 
 };
 
