c77c56e746f26219b0e34f53d14bde648b8297bc
Tobainax <tobiasmartin2@hotmail.com>
2025-06-15 18:28:01 -0300

    A

--- include/IAltaUsuario.h
+++ include/IAltaUsuario.h
@@ -2,10 +2,14 @@
 #define IALTAUSUARIO_H
 
 #include "DTFecha.h"
+#include <string>
+#include <set>
+#include "Usuario.h"
+#include "TipoTecho.h"
 
 class IAltaUsuario {
     public:
-        virtual void guardarReferencia(Usuario::Usuario u) = 0;
+        virtual void guardarReferencia(Usuario* u) = 0;
         virtual Usuario* getUtemp() = 0;
         virtual bool altaCliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento) = 0;
         virtual bool altaPropietario(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string cuentaBancaria, std::string telefono) = 0;
