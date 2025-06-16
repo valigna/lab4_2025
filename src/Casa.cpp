e638d633954634517f79af325a09792b30a997ed
Tobainax <tobiasmartin2@hotmail.com>
2025-06-15 23:18:14 -0300

    A

--- src/Casa.cpp
+++ src/Casa.cpp
@@ -3,5 +3,5 @@
 Casa::Casa(int codigo, std::string direccion, int numeroPuerta, int superficie, int anoConstruccion, bool esPH, TipoTecho techo)
     : Inmueble(codigo, direccion, numeroPuerta, superficie, anoConstruccion),
     esPH(esPH),
-    TipoTecho(techo)
+    techo(techo)
 {}
