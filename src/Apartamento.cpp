18afcd29b657d98fa77a3c0f06896b51179dd106
Tobainax <tobiasmartin2@hotmail.com>
2025-06-15 23:17:48 -0300

    A

--- src/Apartamento.cpp
+++ src/Apartamento.cpp
@@ -1,6 +1,6 @@
 #include "../include/Apartamento.h"
 
-Apartamento(int codigo, std::string direccion, int numeroPuerta, int superficie, int anoConstruccion, int piso, bool tieneAscensor, float gastosComunes)
+Apartamento::Apartamento(int codigo, std::string direccion, int numeroPuerta, int superficie, int anoConstruccion, int piso, bool tieneAscensor, float gastosComunes)
     : Inmueble(codigo, direccion, numeroPuerta, superficie, anoConstruccion),
     piso(piso),
     tieneAscensor(tieneAscensor),
--- src/Casa.cpp
+++ src/Casa.cpp
@@ -1,6 +1,6 @@
 #include "../include/Casa.h"
 
-Casa(int codigo, std::string direccion, int numeroPuerta, int superficie, int anoConstruccion, bool esPH, TipoTecho techo)
+Casa::Casa(int codigo, std::string direccion, int numeroPuerta, int superficie, int anoConstruccion, bool esPH, TipoTecho techo)
     : Inmueble(codigo, direccion, numeroPuerta, superficie, anoConstruccion),
     esPH(esPH),
     TipoTecho(techo)
