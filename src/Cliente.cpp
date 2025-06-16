dac619e4d126b6b1cded3d68b3e206b2a7d8ae0d
Tobainax <tobiasmartin2@hotmail.com>
2025-06-15 22:49:33 -0300

    A

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -1,6 +1,6 @@
 #include "../include/Cliente.h"
 
-Cliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento){
+Cliente::Cliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento){
     this->nickname=nickname;
     this->contrasena=contrasena;
     this->nombre=nombre;
