d762d9d241e7c7431c0f99d9c2ab44c563e199b6
Tobainax <tobiasmartin2@hotmail.com>
2025-06-15 22:55:24 -0300

    A

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -1,8 +1,8 @@
 #include "../include/Cliente.h"
 
-Cliente::Cliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento){
+Cliente::Cliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento)
     : Usuario(nickname, contrasena, nombre, email),
     apellido(apellido),
     documento(documento)
-}
+
 
