0cedce5d765f1637e2c50c75b1ffedd5f19e5914
Tobainax <tobiasmartin2@hotmail.com>
2025-06-16 00:33:12 -0300

    A

--- src/ControllerPublicacion.cpp
+++ src/ControllerPublicacion.cpp
@@ -4,6 +4,7 @@
 #include "../include/Inmueble.h"
 #include "../include/Inmobiliaria.h"
 #include "../include/AdministraPropiedad.h"
+#include "../include/ColeccionUsuario.h"
 
 ControllerPublicacion& ControllerPublicacion::getInstancia(){
     if (instancia==NULL){
@@ -29,8 +30,8 @@ bool ControllerPublicacion::AltaPublicacion(std::string nicknameInmobiliaria, in
         int c=ControllerPublicacion::actualizarCodigoUP();
         Publicacion* p= new Publicacion(c, f, tipoPublicacion, texto, precio, false);
         ap->getPublicaciones().insert(p);
-        ControllerPublicacion::getInstancia().publicaciones.insert{c, p}; 
-        p.setAP(ap);
+        ControllerPublicacion::getInstancia().publicaciones.insert(c, p); 
+        p->setAP(ap);
         if (tipoPublicacion==Venta){
             if (ap->getPVentaActiva()==NULL){
                 p->activar();
@@ -45,18 +46,19 @@ bool ControllerPublicacion::AltaPublicacion(std::string nicknameInmobiliaria, in
         else {
             if (ap->getPAlquilerActiva()==NULL){
                 p->activar();
-                ap->setAlquileraActiva(p);
+                ap->setPAlquileraActiva(p);
             }
             else if (ap->getAlquilerActiva()->getDTFecha()->operator<(f)){
-                ap->getAlquilerActiva()->desactivar();
+                ap->getPAlquilerActiva()->desactivar();
                 p->activar();
-                ap->setAlquileraActiva(p);
+                ap->setPAlquileraActiva(p);
             }
         }
+    }
     return true;
 }
 
-std::set<DTPublicacion> ControllerPublicacion::listarPublicacion(TipoPublicacion tipoPublicacion, float precionMinimo, float precioMaximo, TipoInmueble tipoInmueble){  
+std::set<DTPublicacion*> ControllerPublicacion::listarPublicacion(TipoPublicacion tipoPublicacion, float precionMinimo, float precioMaximo, TipoInmueble tipoInmueble){  
     std::set<DTPublicacion> resultado;
     for (Publicacion* pub : this->publicaciones) {
         if (pub->cumpleFiltros(tipoPublicacion, precioMinimo, precioMaximo) &&
