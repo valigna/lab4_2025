67e3dca2f2419cf026c481f36b4c413b5ba5101d
Tobainax <tobiasmartin2@hotmail.com>
2025-06-16 00:19:46 -0300

    A

--- src/ControllerPublicacion.cpp
+++ src/ControllerPublicacion.cpp
@@ -18,42 +18,41 @@ int ControllerPublicacion::actualizarCodigoUP(){
 }
 
 bool ControllerPublicacion::AltaPublicacion(std::string nicknameInmobiliaria, int codigoInmueble, TipoPublicacion tipoPublicacion, std::string texto, float precio){
-    Inmobiliaria* i=Factory::getInstance()->getAltaUsuario()->findUsuario(nicknameInmobiliaria);
+    Inmobiliaria* i=Factory::getInstance()->getColeccionUsuario()->findUsuario(nicknameInmobiliaria);
     AdministraPropiedad* ap=i->getAP(codigoInmueble);
     DTFecha* f= Factory::getInstance()->getControladorFechaActual()->getFechaActual();
-    if (ap.existeTipoPublicacionActual(tipoPublicacion)){
+    if (ap->existeTipoPublicacionActual(tipoPublicacion)){
         return false;
     }
     else {
         int c=ControllerPublicacion::actualizarCodigoUP();
-        Publicacion p(c, f, tipoPublicacion, texto, precio, false);
-        ap.getPublicaciones().insert(p);
-        ControllerPublicacion::getInstancia().publicaciones.insert{c, *p}; 
+        Publicacion* p= new publicacion(c, f, tipoPublicacion, texto, precio, false);
+        ap->getPublicaciones().insert(p);
+        ControllerPublicacion::getInstancia().publicaciones.insert(p)}; 
         p.setAP(ap);
         if (tipoPublicacion==Venta){
-            if (ap.getPVentaActiva()==NULL){
-                p.activar();
-                ap.setPVentaActiva(p);
+            if (ap->getPVentaActiva()==NULL){
+                p->activar();
+                ap->setPVentaActiva(p);
             }
-            else if (ap.getPVentaActiva().getDTFecha()->operator<(f)){
-                ap.getPVentaActiva().desactivar();
-                p.activar();
-                ap.setPVentaActiva(p);
+            else if (ap->getPVentaActiva()->getDTFecha()->operator<(f)){
+                ap->getPVentaActiva()->desactivar();
+                p->activar();
+                ap->setPVentaActiva(p);
             }
         }
         else {
-            if (ap.getPAlquilerActiva()==NULL){
-                p.activar();
-                ap.setAlquileraActiva(p);
+            if (ap->getPAlquilerActiva()==NULL){
+                p->activar();
+                ap->setAlquileraActiva(p);
             }
-            else if (ap.getAlquilerActiva().getDTFecha()->operator<(f)){
-                ap.getAlquilerActiva().desactivar();
-                p.activar();
-                ap.setAlquileraActiva(p);
+            else if (ap->getAlquilerActiva()->getDTFecha()->operator<(f)){
+                ap->getAlquilerActiva()->desactivar();
+                p->activar();
+                ap->setAlquileraActiva(p);
             }
         }
-        return true;
-    }
+    return true;
 }
 
 std::set<DTPublicacion> ControllerPublicacion::listarPublicacion(TipoPublicacion tipoPublicacion, float precionMinimo, float precioMaximo, TipoInmueble tipoInmueble){  
