6c3fdaff7a99e9012291ff5b07969254e18dbd45
Tobainax <tobiasmartin2@hotmail.com>
2025-06-21 16:11:07 -0300

    a

--- include/IControllerInmueble.h
+++ include/IControllerInmueble.h
@@ -2,7 +2,9 @@
 #define ICONTROLLERINMUEBLE_H
 
 #include <string>
+#include <set>
 #include "TipoTecho.h"
+#include <DTInmuebleListado.h>
 
 class IControllerInmueble{
     public:
@@ -10,10 +12,8 @@ class IControllerInmueble{
         virtual void eliminarInmueble(int codigoInmueble) = 0;
         virtual void AltaCasa(std::string direccion, int numeroPuerta, int superficie, int anoConstruccion, bool esPH, TipoTecho techo) = 0;
         virtual void AltaApartamento(std::string direccion, int numeroPuerta, int superficie, int anoConstruccion, int piso, bool tieneAscensor, float gastosComunes) = 0;
- virtual std::set(DTInmuebleListado*) listarInmuebles() = 0;
-   virtual DTInmueble* detalleInmueble(codigoInmueble int) = 0;
-};
-
+        virtual std::set(DTInmuebleListado*) listarInmuebles() = 0;
+        virtual DTInmueble* detalleInmueble(codigoInmueble int) = 0;
 };
 
 #endif
