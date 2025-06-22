0de1f83821a0061e0c4f01a65624dd0419cd55da
Tobainax <tobiasmartin2@hotmail.com>
2025-06-22 20:51:29 -0300

    a

--- include/IControllerPublicacion.h
+++ include/IControllerPublicacion.h
@@ -14,6 +14,8 @@ class IControllerPublicacion {
         virtual int actualizarCodigoUP() = 0;
         virtual std::set<DTPublicacion*> listarPublicacion(TipoPublicacion tipoPublicacion, float precioMinimo, float precioMaximo, TipoInmueble tipoInmueble) = 0;
         virtual DTInmueble* detalleInmueblePublicacion(int codigoPublicacion) = 0;
+        virtual void eliminarPublicacion(int codigoPublicacion) = 0;
+        virtual Publicacion* getPublicacion(int codigo) = 0;
 };
 
 #endif
